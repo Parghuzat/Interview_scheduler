47b73662983a518ea72e6259fe62397966dff801
Parghuzat <paguzati@hotmail.com>
2021-12-11 14:02:43 -0800

    button completed

---
+++ src/components/DayList.js
@@ -0,0 +1,16 @@
+import { obj } from "pumpify";
+import React from "react";
+import DayListItem from "./DayListItem";
+
+export default function DayList(props) {
+  let dayListItems = props.days.map((obj) => 
+    <DayListItem key={obj.id} name={obj.name} spots={obj.spots} setDay={props.setDay} selected={obj.name === props.day}/>
+    );
+  
+return (
+  <ul>
+     {dayListItems}
+  </ul>
+ 
+)
+};
--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -7,10 +7,22 @@ export default function DayListItem(props) {
     "day-list__item--selected": props.selected === true,
     "day-list__item--full": props.spots === 0
   });
+
+  const formatSpots = (props) => {
+    if (props.spots === 0) {
+      return "no spots";
+    }
+    if (props.spots === 1) {
+      return "1 spot";
+    }
+    return props.spots + " spots";
+  }
+
+
   return (
     <li className={dayClass} onClick={() => props.setDay(props.name)}>
-      <h2 className="text--regular">Day Name</h2> 
-      <h3 className="text--light">X spots remaining</h3>
+      <h2 className="text--regular">{props.name}</h2> 
+      <h3 className="text--light">{formatSpots(props)} remaining</h3>
     </li>
   );
 }

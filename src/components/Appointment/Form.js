164e970ae160fd9d6d4c230c2ca77121cdfa5f3b
Parghuzat <paguzati@hotmail.com>
2021-12-12 01:14:06 -0800

    Controller component in Form.js

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -1,8 +1,15 @@
-import React from "react";
+import React, {useState} from "react";
 import Button from "components/Button";
 import InterviewerList from "components/InterviewerList";
 
+
+
 export default function Form (props) {
+
+  const [student, setStudent] = useState(props.student || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+
+  
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
@@ -14,18 +21,19 @@ export default function Form (props) {
             placeholder="Enter Student Name"
             /*
               This must be a controlled component
-              your code goes here
+              your code goes here1
             */
           />
         </form>
-        <InterviewerList 
+        <InterviewerList
+          interviewers={props.interviewers}
           /* your code goes here */
         />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger {/* your code goes here */}>Cancel</Button>
-          <Button confirm {/* your code goes here */}>Save</Button>
+          <Button danger onClick={props.onCancel}>Cancel</Button>
+          <Button confirm onClick={props.onSave}>Save</Button>
         </section>
       </section>
 </main>

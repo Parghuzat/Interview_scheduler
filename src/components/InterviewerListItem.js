d95e5057efe298d9dd5e07f08657bbf85502d582
Parghuzat <paguzati@hotmail.com>
2021-12-11 18:06:44 -0800

    updated interviewerlistitem

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -16,13 +16,14 @@ export default function InterviewerListItem (props) {
 
 
   return (
-    <li className={interviewerClass} onClick={props.setInterviewer}>
+    <li className={interviewer} onClick={() => props.setInterviewer(props.id)}>
       <img
         className="interviewers__item-image"
         src={props.avatar}
         alt={props.name}
       />
-      {props.selected && props.name}
+      {name}
+  
     </li>
   );
 }
